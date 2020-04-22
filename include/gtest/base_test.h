5b1f3f0e81204658a4ff8d8766ef37f4c0bbbb7d
Paweł Witak <hensoft.pawelwitak@gmail.com>
2020-04-22 14:15:02 +0200

    Cosmetic improvements in UTs project

--- include/gtest/base_test.h
+++ include/gtest/base_test.h
@@ -1,13 +1,33 @@
+/**
+ *******************************************************************************
+ * @file     base_test.h
+ * @author   HENIUS (Pawe³ Witak)
+ * @version  1.0.0
+ * @date     22-04-2020
+ * @brief    Extensions for gtest library (header file)
+ *******************************************************************************
+ *
+ * <h2><center>COPYRIGHT 2020 HENIUS</center></h2>
+ */
+
 #pragma once
+
+/* Include section -----------------------------------------------------------*/
+
+// --->System files
+
+#include <gtest/gtest.h>
+
+/* Macros, constants and definitions section ---------------------------------*/
 
 #ifndef TEST_CLASS_NAME
 	#error "Test class name is not specified"
 #endif
 
-#include <gtest/gtest.h>
+/*! Macro to create test class */
+#define TEST_CLASS()  		class TEST_CLASS_NAME : public ::testing::Test
 
-#define TEST_CLASS() \
-	class TEST_CLASS_NAME : public ::testing::Test
+/*! Macro to create unit test */
+#define UNIT_TEST(testName)	TEST_F(TEST_CLASS_NAME, testName)
 
-#define UNIT_TEST(testName) \
-	TEST_F(TEST_CLASS_NAME, testName)
+/******************* (C) COPYRIGHT 2020 HENIUS *************** END OF FILE ****/
