770c787fc9566b990adae425dc9b66d5d08f3327
Paweł Witak <hensoft.pawelwitak@gmail.com>
2020-04-23 12:36:25 +0200

    UTs library improvements

--- include/gtest/base_test.h
+++ include/gtest/base_test.h
@@ -2,8 +2,8 @@
  *******************************************************************************
  * @file     base_test.h
  * @author   HENIUS (Pawe³ Witak)
- * @version  1.0.0
- * @date     22-04-2020
+ * @version  1.0.1
+ * @date     23-04-2020
  * @brief    Extensions for gtest library (header file)
  *******************************************************************************
  *
@@ -20,14 +20,32 @@
 
 /* Macros, constants and definitions section ---------------------------------*/
 
-#ifndef TEST_CLASS_NAME
-	#error "Test class name is not specified"
-#endif
-
 /*! Macro to create test class */
-#define TEST_CLASS()  		class TEST_CLASS_NAME : public ::testing::Test
+#define TEST_CLASS(className) \
+	TEST_CLASS_NAME : public ::testing::Test
 
-/*! Macro to create unit test */
-#define UNIT_TEST(testName)	TEST_F(TEST_CLASS_NAME, testName)
+/*! Macro to create unit test with fixture class*/
+#define UNIT_TEST_F(className, testName)	\
+	TEST_F(className, testName)
+
+/*! Macro to create unit test with no fixture class*/
+#define UNIT_TEST(testName)	\
+	TEST_F(testName##_class, testName)
+
+/*! Macro to get test class name for prametrized test */
+#define TEST_CLASS_NAME_WITH_PARAM(testName) \
+	testName##_class
+
+/*! Macro to create class for test with params */
+#define TEST_CLASS_WITH_PARAM(testName, paramType) \
+	TEST_CLASS_NAME_WITH_PARAM(testName) : public ::testing::TestWithParam<paramType>
+
+/*! Macro to create unit test with parameter */
+#define UNIT_TEST_WITH_PARAM(testName, ...) \
+	INSTANTIATE_TEST_CASE_P( \
+			testName##_sequence, \
+			testName##_class, \
+			::testing::Values(__VA_ARGS__)); \
+	TEST_P(testName##_class, testName)
 
 /******************* (C) COPYRIGHT 2020 HENIUS *************** END OF FILE ****/
